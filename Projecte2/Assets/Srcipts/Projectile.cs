6fbacd8aef4c6db6e4e8e391499f32501a79865b
Jaume Vidal Torrents <jaume.vidal@enti.cat>
2024-09-22 13:18:24 +0200

    Shoot enemy

--- Projecte2/Assets/Srcipts/Projectile.cs
+++ Projecte2/Assets/Srcipts/Projectile.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
+    [SerializeField] private float speed;
+    private Transform player;
+    private Rigidbody2D controller;
     // Start is called before the first frame update
     void Start()
     {
+        player = FindObjectOfType<PlayerCtrl>().transform;
+        controller = GetComponent < Rigidbody2D>();
+        LaunchProjectile();
+    }
+    //Calculate the distance between de projectile and the player creating a vector
+    private void LaunchProjectile()
+    {
+        Vector2 directionToPlayer= (player.position - transform.position).normalized;
+        controller.velocity=directionToPlayer*speed;
         
+
+
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        // Verify the colision
+        if (collision.CompareTag("Player"))
+        {
+            Destroy(gameObject); 
+        }
+        else
+        {
+            Destroy(gameObject); // También destruye el proyectil
+        }
     }
+
 }
--- Projecte2/Assets/Srcipts/ShootIA.cs
+++ Projecte2/Assets/Srcipts/ShootIA.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class ShootIA : MonoBehaviour
 {
+    [SerializeField] private GameObject ProjectilePrefab;
+    [SerializeField] private float timeBetweenShoots;   
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(Shoot());
     }
 
     // Update is called once per frame
-    void Update()
+    IEnumerator Shoot()
     {
+        while(true)
+        {
+            yield return new WaitForSeconds(timeBetweenShoots);
+            Instantiate(ProjectilePrefab, transform.position, Quaternion.identity);
+        }
         
     }
 }
